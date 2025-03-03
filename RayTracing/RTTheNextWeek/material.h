1ed945088b5e3a01f3031ce011ad187521ee2a82
feixus <feixusuw@hotmail.com>
2025-03-03 20:13:06 +0800

    ray tracing: volume

--- RayTracing/RTTheNextWeek/material.h
+++ RayTracing/RTTheNextWeek/material.h
@@ -109,4 +109,20 @@ namespace My {
         private:
             std::shared_ptr<texture> tex;
     };
+
+    class isotropic : public material {
+        public:
+            isotropic(const color& albedo) : tex(std::make_shared<solid_color>(albedo)) {}
+
+            isotropic(std::shared_ptr<texture> tex) : tex(tex) {}
+
+            bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+                scattered = ray(rec.p, random_unit_vector(), r_in.time());
+                attenuation = tex->value(rec.u, rec.v, rec.p);
+                return true;
+            }
+
+        private:
+            std::shared_ptr<texture> tex;
+    };
 }
