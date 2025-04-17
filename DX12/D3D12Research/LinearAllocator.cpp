ce3ffbdfffef6be7bd40b94dfc7473234e540b74
feixus <feixusu@hotmail.com>
2025-04-17 23:43:54 +0800

    DX12: additions

--- DX12/D3D12Research/LinearAllocator.cpp
+++ DX12/D3D12Research/LinearAllocator.cpp
@@ -1,4 +1,5 @@
 #include "LinearAllocator.h"
+#include "Graphics.h"
 
 LinearAllocator::LinearAllocator(Graphics* pGraphics)
 {
@@ -35,15 +36,74 @@ DynamicAllocation LinearAllocator::Allocate(const LinearAllocationType type, siz
 
 LinearAllocationPage* LinearAllocatorPageManager::RequestPage()
 {
-	while (!m_RetiredPages.empty())
+	while (!m_RetiredPages.empty() && m_pGraphics->IsFenceComplete(m_RetiredPages.front().first))
 	{
-
+		m_AvailablePages.push(m_RetiredPages.front().second);
+		m_RetiredPages.pop();
 	}
 
-	return nullptr;
+	LinearAllocationPage* pPage = nullptr;
+
+	if (!m_AvailablePages.empty())
+	{
+		pPage = m_AvailablePages.front();
+		m_AvailablePages.pop();
+	}
+	else
+	{
+		pPage = CreateNewPage(m_Type == LinearAllocationType::CpuWrite ? CPU_PAGE_SIZE : GPU_PAGE_SIZE);
+		m_PagePool.emplace_back(pPage);
+	}
+
+	return pPage;
 }
 
 LinearAllocationPage* LinearAllocatorPageManager::CreateNewPage(const size_t size)
 {
-	return nullptr;
+	D3D12_HEAP_PROPERTIES heapProps;
+	heapProps.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+	heapProps.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+	heapProps.CreationNodeMask = 1;
+	heapProps.VisibleNodeMask = 1;
+
+	D3D12_RESOURCE_DESC resourceDesc;
+	resourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resourceDesc.Alignment = 0;
+	resourceDesc.Height = 0;
+	resourceDesc.DepthOrArraySize = 1;
+	resourceDesc.Format = DXGI_FORMAT_UNKNOWN;
+	resourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+	resourceDesc.MipLevels = 1;
+	resourceDesc.SampleDesc.Count = 1;
+	resourceDesc.SampleDesc.Quality = 0;
+
+	D3D12_RESOURCE_STATES usage;
+
+	if (m_Type == LinearAllocationType::GpuExclusive)
+	{
+		resourceDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS;
+		resourceDesc.Width = size ? size : GPU_PAGE_SIZE;
+		heapProps.Type = D3D12_HEAP_TYPE_DEFAULT;
+		usage = D3D12_RESOURCE_STATE_UNORDERED_ACCESS;
+	}
+	else
+	{
+		resourceDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
+		resourceDesc.Width = size ? size : CPU_PAGE_SIZE;
+		heapProps.Type = D3D12_HEAP_TYPE_UPLOAD;
+		usage = D3D12_RESOURCE_STATE_GENERIC_READ;
+	}
+
+	ID3D12Resource* pResource = nullptr;
+	m_pGraphics->GetDevice()->CreateCommittedResource(
+		&heapProps,
+		D3D12_HEAP_FLAG_NONE,
+		&resourceDesc,
+		usage,
+		nullptr,
+		IID_PPV_ARGS(&pResource));
+
+	pResource->SetName(L"Linear Allocator Page");
+
+	return new LinearAllocationPage(pResource, (size_t)resourceDesc.Width, usage);
 }
