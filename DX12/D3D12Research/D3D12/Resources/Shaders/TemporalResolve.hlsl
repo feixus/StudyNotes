d713e1e4b09ca25ae09d0200bb7e5132542e7d6d
feixus <feixusuw@hotmail.com>
2025-10-10 13:44:04 +0800

    DX12: catmull rom filter for TAA

--- DX12/D3D12Research/D3D12/Resources/Shaders/TemporalResolve.hlsl
+++ DX12/D3D12Research/D3D12/Resources/Shaders/TemporalResolve.hlsl
@@ -4,8 +4,9 @@
 #define TAA_REPROJECT
 #define TAA_AABB_ROUNDED
 #define TAA_NEIGHBORHOOD_CLIP
-#define TAA_VELOCITY_CORRECT
+//#define TAA_VELOCITY_CORRECT
 #define TAA_TONEMAP
+#define TAA_RESOLVE_CATMULL_ROM
 
 #define RootSig "CBV(b0, visibility=SHADER_VISIBILITY_ALL), " \
                 "DescriptorTable(UAV(u0, numDescriptors = 1), visibility = SHADER_VISIBILITY_ALL), " \
@@ -56,6 +57,54 @@ float3 SampleColor(Texture2D texture, SamplerState samplerState, float2 texCoord
     return texture.SampleLevel(samplerState, texCoord, 0).rgb;
 }
 
+// samples a texture with Catmull-Rom filtering, using 9 texture fetches instead of 16
+float4 SampleTextureCatmullRom(in Texture2D tex, in SamplerState linearSampler, in float2 uv, in float2 texSize)
+{
+    // sample a 4x4 grid of texels surrounding the target UV coordinate.
+    // do this by rounding down the sample location to get the exact center texel.
+    float2 samplePos = uv * texSize;
+    float2 texPos1 = floor(samplePos - 0.5f) + 0.5f;
+
+    // compute the fractional offset from the center texel to original sample location.
+    // which will feed into the Catmull-Rom filter to get filter weights.
+    float2 frac = samplePos - texPos1;
+
+    // compute the Catmull-Rom weights using the fractional offset. (four control points: P0, P1, P2, P3)
+    float2 w0 = frac * (-0.5f + frac * (1.0f - 0.5f * frac));
+    float2 w1 = 1.0f + frac * frac * (-2.5f + 1.5f * frac);
+    float2 w2 = frac * (0.5f + frac * (2.0f - 1.5f * frac));
+    float2 w3 = frac * frac * (-0.5f + 0.5f * frac);
+
+    // work out weighting factors and sampling offsets that will let us use bilinear filtering to
+    // simultaneously evaluate the middle 2 samples(P1 and P2) from the 4x4 grid. so only need 9 samples(3x3)
+    float2 w12 = w1 + w2;
+    float2 offset12 = w2 / (w1 + w2);
+
+    // compute the final UV coordinates we'll use for sampling the texture. 
+    float2 texPos0 = texPos1 - 1;
+    float2 texPos3 = texPos1 + 2;
+    float2 texPos12 = texPos1 + offset12;
+
+    texPos0 /= texSize;
+    texPos3 /= texSize;
+    texPos12 /= texSize;
+
+    float4 result = 0.0f;
+    result += tex.SampleLevel(linearSampler, float2(texPos0.x, texPos0.y), 0.0f) * w0.x * w0.y;
+    result += tex.SampleLevel(linearSampler, float2(texPos12.x, texPos0.y), 0.0f) * w12.x * w0.y;
+    result += tex.SampleLevel(linearSampler, float2(texPos3.x, texPos0.y), 0.0f) * w3.x * w0.y;
+
+    result += tex.SampleLevel(linearSampler, float2(texPos0.x, texPos12.y), 0.0f) * w0.x * w12.y;
+    result += tex.SampleLevel(linearSampler, float2(texPos12.x, texPos12.y), 0.0f) * w12.x * w12.y;
+    result += tex.SampleLevel(linearSampler, float2(texPos3.x, texPos12.y), 0.0f) * w3.x * w12.y;
+
+    result += tex.SampleLevel(linearSampler, float2(texPos0.x, texPos3.y), 0.0f) * w0.x * w3.y;
+    result += tex.SampleLevel(linearSampler, float2(texPos12.x, texPos3.y), 0.0f) * w12.x * w3.y;
+    result += tex.SampleLevel(linearSampler, float2(texPos3.x, texPos3.y), 0.0f) * w3.x * w3.y;
+
+    return result;
+}
+
 [RootSignature(RootSig)]
 [numthreads(8, 8, 1)]
 void CSMain(uint3 DispatchThreadId : SV_DispatchThreadID)
@@ -75,7 +124,15 @@ void CSMain(uint3 DispatchThreadId : SV_DispatchThreadID)
     prevPos.xyz /= prevPos.w;
     velocity = (prevPos - pos).xy - cParameters.Jitter * cParameters.InvScreenDimensions;
 #endif
+
+#ifdef TAA_RESOLVE_CATMULL_ROM
+    float2 dimensions;
+    tPreviousColor.GetDimensions(dimensions.x, dimensions.y);
+    float3 prevColor = SampleTextureCatmullRom(tPreviousColor, sLinearSampler, texCoord + velocity, dimensions).rgb;
+#else
     float3 prevColor = SampleColor(tPreviousColor, sLinearSampler, texCoord + velocity);
+#endif
+
 
 #if defined(TAA_NEIGHBORHOOD_CLAMP) || defined(TAA_NEIGHBORHOOD_CLIP)
     float3 lt = SampleColor(tCurrentColor, sPointSampler, texCoord + pixelSize * float2(-1, -1));
